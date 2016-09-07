2bde1d0392210f2178d440a9094e8b8bb02b829a
gsekar <gsekar@DIN76000171.corp.capgemini.com>
2016-09-07 18:14:36 +0530

    commit1

---
+++ GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,18 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	private static void greetings() {
+		System.out.println("Helloo!!");
+		
+}
+	
+	public static void main(String[] args) {
+		
+		SalesClass sc=new SalesClass();
+		sc.display();
+		greetings();
+		
+	}
+
+}
---
+++ GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SalesClass {
+	
+	int[] arr={1,2,3,4};
+	
+	public void display(){
+		System.out.println("Hi!!! Sales Method");
+	}
+
+}
